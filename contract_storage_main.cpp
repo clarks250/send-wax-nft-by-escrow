38589ae0e50850c2354c7983d02402601027c05f
clarks <leiferemil@gmail.com>
2023-11-15 02:42:53 +0100

    Another contract structure integration

--- contract_storage_main.cpp
+++ contract_storage_main.cpp
@@ -1,11 +1,5 @@
 #include <eosio/eosio.hpp>
-#include <eosio/print.hpp>
-#include <eosio/asset.hpp>
-#include <eosio/system.hpp>
-#include <eosio/symbol.hpp>
-#include <string>
-#include <eosio/crypto.hpp>
-#include <eosio/transaction.hpp>
+#include <vector>
 
 using namespace eosio;
 
@@ -13,103 +7,102 @@ CONTRACT nft_contract : public eosio::contract {
 public:
     using contract::contract;
 
-    nft_contract(name receiver, name code, eosio::datastream<const char*> ds)
-        : contract(receiver, code, ds), trans_table(receiver, receiver.value), nfts_table(receiver, receiver.value)
-    {}
+    // Define escrows and min_escrows at the beginning of the contract
+    std::vector<name> escrows;
+    uint32_t min_escrows;
 
-   [[eosio::action]]
-void sendnft(const name& sender, const name& recipient, const std::vector<name>& approvers, uint64_t nft_id) {
-    require_auth(sender);
-    eosio::print("Sending NFT from ", sender, " to ", recipient, " with NFT ID ", nft_id);
+    [[eosio::on_notify("atomicassets::transfer")]]
+    void receiveasset(name from, name to, std::vector<uint64_t> asset_ids, std::string memo) {
+        if (to != get_self())
+            return;
 
-    uint64_t transaction_id = current_time_point().time_since_epoch().count();
+        check(asset_ids.size() == 1, "Error: Number of tokens must be exactly 1");
 
-    std::string transaction_data = std::to_string(sender.value) + std::to_string(recipient.value) + std::to_string(nft_id);
-    for (const auto& approver : approvers) {
-        transaction_data += std::to_string(approver.value);
-    }
-    checksum256 transaction_hash = sha256(transaction_data.c_str(), transaction_data.size());
+        for (const auto& asset_id : asset_ids) {
+            auto existing_nft = nft_table_inst.find(asset_id);
 
-    trans_table.emplace(get_self(), [&](auto& row) {
-        row.id = transaction_id;
-        row.sender = sender;
-        row.recipient = recipient;
-        row.approvers = approvers;
-        row.transaction_hash = transaction_hash;
-        row.nft_id = nft_id;
-    });
+            if (existing_nft != nft_table_inst.end() && existing_nft->sender == from) {
+                nft_table_inst.modify(existing_nft, get_self(), [&](auto &row) {
+                    row.recipient = to;
+                    row.escrows = escrows;
+                    row.min_escrows = min_escrows;
+                });
 
-    action(
-        permission_level{get_self(), "active"_n},
-        "atomicassets"_n,
-        "transfer"_n,
-        std::make_tuple(sender, name("storagetest1"), nft_id, "sent from senderacount to storagetest1")
-    ).send();
-
-    for (const auto& approver : approvers) {
-        eosio::action(
-            eosio::permission_level{get_self(), eosio::name("active")},
-            get_self(),
-            eosio::name("notify"),
-            std::make_tuple(sender, recipient, approver, transaction_id, transaction_hash)
-        ).send();
-    }
-}
-
-    [[eosio::action]]
-    void approve(const name& approver, uint64_t transaction_id, const checksum256& transaction_hash) {
-        require_auth(approver);
-        eosio::print("Approving transaction with ID ", transaction_id, " by ", approver);
-
-        auto transaction_itr = trans_table.find(transaction_id);
-        eosio::check(transaction_itr != trans_table.end(), "Transaction does not exist");
-        eosio::check(transaction_itr->approved_by.find(approver) == transaction_itr->approved_by.end(), "Transaction already approved by this account");
-        eosio::check(transaction_itr->transaction_hash == transaction_hash, "Transaction data has been tampered with");
-
-        trans_table.modify(transaction_itr, get_self(), [&](auto& row) {
-            row.approved_by.insert(approver);
-        });
-
-        if (transaction_itr->approved_by.size() == transaction_itr->approvers.size()) {
-            // Размораживаем NFT и автоматически передаем его конечному отправителю
-            auto nft_itr = nfts_table.find(transaction_itr->nft_id);
-            eosio::check(nft_itr != nfts_table.end(), "NFT not found");
-            
-            nfts_table.modify(nft_itr, get_self(), [&](auto& row) {
-                row.owner = transaction_itr->recipient;
-                row.frozen = false;
-            });
+                print("NFT successfully added to the table. Sender: ", from, ", Asset ID: ", asset_id);
+            } else {
+                print("Error: NFT not found or sender does not match.");
+            }
         }
     }
 
-    [[eosio::action]]
-    void notify(const name& sender, const name& recipient, const name& approver, uint64_t transaction_id, const checksum256& transaction_hash) {
-        require_auth(get_self());
-        eosio::print("Notifying sender, recipient, and approver about transaction with ID ", transaction_id);
+    ACTION updateinfo(name sender, uint64_t asset_id, name recipient, std::vector<name> escrows, uint32_t min_escrows) {
+        require_auth(sender);
+
+        nft_table nft_table_inst(get_self(), get_self().value);
+        auto existing_nft = nft_table_inst.find(asset_id);
+
+        check(existing_nft != nft_table_inst.end(), "Error: NFT not found");
+        check(existing_nft->sender == sender, "Error: Sender does not match");
+
+        nft_table_inst.modify(existing_nft, get_self(), [&](auto &row) {
+            row.recipient = recipient;
+            row.escrows = escrows;
+            row.min_escrows = min_escrows;
+        });
+
+        print("Transaction information successfully updated. Asset ID: ", asset_id);
+    }
+
+    ACTION signescrow(name escrow, uint64_t asset_id) {
+        require_auth(escrow);
+
+        nft_table nft_table_inst(get_self(), get_self().value);
+        auto existing_nft = nft_table_inst.find(asset_id);
+
+        check(existing_nft != nft_table_inst.end(), "Error: NFT not found");
+
+        nft_table_inst.modify(existing_nft, get_self(), [&](auto &row) {
+            // Check that escrow is not already signed
+            auto it = std::find(row.signed_escrows.begin(), row.signed_escrows.end(), escrow);
+            check(it == row.signed_escrows.end(), "Error: Escrow already signed");
+
+            // Sign the escrow
+            row.signed_escrows.push_back(escrow);
+            row.signed_escrows_count++;
+
+            // Check if the minimum number of signatures is reached
+            if (row.signed_escrows_count >= row.min_escrows) {
+                // Perform action for automatic transfer
+                // In the example, sending a transaction to atomicassets::transfer
+                action(
+                    permission_level{get_self(), "active"_n},
+                    "atomicassets"_n,
+                    "transfer"_n,
+                    std::make_tuple(row.sender, row.recipient, row.asset_id, "")
+                ).send();
+
+                // Reset signatures for the next transactions
+                row.signed_escrows.clear();
+                row.signed_escrows_count = 0;
+            }
+        });
+
+        print("Escrow successfully signed. Asset ID: ", asset_id);
     }
 
 private:
-    struct [[eosio::table("transactions")]] tx {
-        uint64_t id;
+    struct [[eosio::table]] nft {
         name sender;
         name recipient;
-        std::vector<name> approvers;
-        checksum256 transaction_hash;
-        uint64_t nft_id;
-        std::set<name> approved_by;
-        uint64_t primary_key() const { return id; }
+        std::vector<name> escrows;
+        std::vector<name> signed_escrows;
+        uint32_t min_escrows;
+        uint32_t signed_escrows_count;
+        uint64_t asset_id;
+
+        uint64_t primary_key() const { return asset_id; }
     };
 
-    typedef eosio::multi_index<name("transactions"), tx> txs;
-    txs trans_table;
-
-    struct [[eosio::table("nfts")]] nft {
-        uint64_t id;
-        name owner;
-        bool frozen;
-        uint64_t primary_key() const { return id; }
-    };
-
-    typedef eosio::multi_index<name("nfts"), nft> nfts;
-    nfts nfts_table;
+    typedef multi_index<"nft"_n, nft> nft_table;
+    nft_table nft_table_inst{get_self(), get_self().value};
 };
